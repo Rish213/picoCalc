6203dd20d016aa05bd164b376d1f832c236aa0b1
Rish213 <rp.rishipawar213@gmail.com>
2025-11-26 01:11:15 +0530

    Startup script improvement

--- start_app.bat
+++ start_app.bat
@@ -1,6 +1,12 @@
 @echo off
-echo Starting picoCalc...
 cd /d "%~dp0"
+echo Starting picoCalc...
+
+if not exist "node_modules" (
+    echo First run detected. Installing dependencies...
+    call npm install
+)
+
 start http://localhost:5173
 echo Starting development server...
 npm run dev
